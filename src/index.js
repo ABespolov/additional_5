85ed233fc2cf92937b2489139a3fb25de1ab7299
oit7 <oit7@belmedpreparaty.com>
2018-09-11 13:02:55 +0300

    featur: improve algorithm

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 module.exports = function check(braces, bracketsConfig) {
-    var matches = {'(': ')', '{': '}', '[': ']'};
+    var matches = {};
     var stack = [];
-    var currentChar;
-
+    for (var i = 0; i < bracketsConfig.length; i++) {
+        matches[bracketsConfig[i][0]] = bracketsConfig[i][1];
+    }
     for (var i = 0; i < braces.length; i++) {
-        currentChar = braces[i];
-
-        if (matches[currentChar]) {
-            stack.push(currentChar);
+        if (matches[braces[i]]) {
+            if (matches[braces[i]] == braces[i] && stack.indexOf(braces[i]) != -1) {
+                var element = stack.pop();
+                if (element != braces[i]) {
+                    return false;
+                }
+            } else {
+                stack.push(braces[i]);
+            }
         } else {
-            if (currentChar !== matches[stack.pop()]) {
+            if (braces[i] != matches[stack.pop()]) {
                 return false;
             }
         }
-    }
 
+    }
     return stack.length === 0;
 }
+
+/*console.log(check('(||)|', [
+    ['|', '|'],
+    ['(', ')']
+]));*/
